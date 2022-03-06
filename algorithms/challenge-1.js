dcceee13867b8b4ccfffae26365653bd72b4aaec
Kingugo007 <95686453+Kingugo007@users.noreply.github.com>
2022-03-06 07:01:33 +0100

    Update challenge-1.js

--- algorithms/challenge-1.js
+++ algorithms/challenge-1.js
@@ -1,3 +1,20 @@
-function missingNumber(numArr) {}
+function missingNumber(numArr) {
+  let missingArr = [];
+  let a = 9; // Math.max(...numArr);
+  let b = 1; //Math.min(...numArr);
 
-module.exports = missingNumber
+  for (let i = b; i <= a; i++) {
+    if (numArr.indexOf(i) < 0) {
+      missingArr.push(i);
+    }
+  }
+  if (missingArr.length === 1) {
+    return missingArr[0];
+  }
+  if (missingArr.length <= 0) {
+    missingArr = false;
+  }
+  return missingArr;
+}
+
+module.exports = missingNumber;
