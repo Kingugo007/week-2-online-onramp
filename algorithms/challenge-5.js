9129c1b325906e16416e646cb06a68f82a270fd3
Kingugo007 <95686453+Kingugo007@users.noreply.github.com>
2022-03-06 07:03:56 +0100

    Update challenge-5.js

--- algorithms/challenge-5.js
+++ algorithms/challenge-5.js
@@ -1,3 +1,13 @@
-function arraySum(arr1, arr2) {}
+function arraySum(arr1, arr2) {
+  let sum = [];
+  let firstLength = arr1.length;
+  let secondLength = arr2.length;
+
+  for(let i = 0; i<firstLength; i++){
+      sum[i] = arr1[i] + arr2[i % secondLength];
+  }
+
+  return sum;
+}
 
 module.exports = arraySum
