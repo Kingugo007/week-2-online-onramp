60290b62a7747e2532536eccb2b1cf6df33b2258
Kingugo007 <95686453+Kingugo007@users.noreply.github.com>
2022-03-06 07:02:17 +0100

    Update challenge-2.js

--- algorithms/challenge-2.js
+++ algorithms/challenge-2.js
@@ -1,3 +1,11 @@
-function numSwap(number) {}
+function numSwap(number) {
+  let numArr = [];
+  number = number.toString().split("");
+  for (let i = 0; i <= number.length; i++) {
+    let newNum = number.splice(0, 2).reverse().join("");
+    numArr.push(newNum);
+  }
+  return parseInt(numArr.join(""));
+}
 
-module.exports = numSwap
+module.exports = numSwap;
